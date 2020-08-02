fc50128c6f35cccb0076a72a1e676c7c04fb2ba6
wanderfalcao <wanderfalcao@hotmail.com>
2020-08-02 20:58:28 -0300

    transformando os resultados de empresa em uma tabela

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservas</title>
+    <title>Bairro Buddy</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -24,13 +24,12 @@
     <div class="container-fluid">
         <nav class="navbar navbar-default navbar-expand-lg ">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#"><b>Nome</b></a>  		
+                <a class="navbar-brand" href="./home.php"><b>Bairro Buddy</b></a>  		
             </div>
             <!-- Collection of nav links, forms, and other content for toggling -->
             <div id="navbarCollapse" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="./home.html">Home</a></li>
-                    <li><a href="./registro_cliente.html">About</a></li>
+                    <li><a href="./home.php">Home</a></li>
                 </ul>                
                 <ul class="nav navbar-nav navbar-right">			
                     <li ><a data-toggle="dropdown" class="dropdown-toggle" href="#">Login</a>
---
+++ index.js
@@ -0,0 +1,114 @@
+function limpa_formulário_cep() {
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+} 
+else {
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+var cep = valor.replace(/\D/g, '');
+
+if (cep != "") {
+
+    var validacep = /^[0-9]{8}$/;
+
+    if(validacep.test(cep)) {
+
+        document.getElementById('rua').value="...";
+        document.getElementById('bairro').value="...";
+        document.getElementById('cidade').value="...";
+        document.getElementById('uf').value="...";
+
+        var script = document.createElement('script');
+
+        script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meu_callback`;
+
+        document.body.appendChild(script);
+
+    } 
+    else {
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} 
+else {
+    limpa_formulário_cep();
+}
+}
+
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i);
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+function validarSenha(senha, confirmarSenha)
+{
+		var senha1 = document.getElementById(senha).value;
+    var senha2 = document.getElementById(confirmarSenha).value;
+        console.log(senha1);
+    if(senha2 == ""){return;}
+    if (senha1 != "" &&
+        senha2 != "" &&
+        senha1 !== senha2)
+        {
+        	alert('senha diferentes');
+        }
+
+}
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tabela");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("td")[n];
+        y = rows[i + 1].getElementsByTagName("td")[n];
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {            
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount ++;
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+  
+
